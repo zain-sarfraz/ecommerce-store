c2fb4a4d16828f6a7a33da315f3863c4d9171102
talhazaryabDA6~~ <zaryabdev404@gmail.com>
2024-10-11 16:34:17 +0500

    Billboard bug fixed

--- components/ui/billboard.tsx
+++ components/ui/billboard.tsx
@@ -7,18 +7,18 @@ interface BillboardProps {
 const Billboard: React.FC<BillboardProps> = ({
   data
 }) => {
-  console
-  return ( 
-    <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
+
+  return (
+    <div className="p-4 overflow-hidden sm:p-6 lg:p-8 rounded-xl">
       <div style={{ backgroundImage: `url(${data?.imageUrl})` }} className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover">
-        <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
-          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
-            {data.label}
+        <div className="flex flex-col items-center justify-center w-full h-full text-center gap-y-8">
+          <div className="max-w-xs text-3xl font-bold sm:text-5xl lg:text-6xl sm:max-w-xl">
+            {data?.label}
           </div>
         </div>
       </div>
     </div>
-   );
+  );
 };
 
 export default Billboard;
